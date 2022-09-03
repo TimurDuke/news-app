a7ce29effefa978d0418c2f3f466b9c158c7bb87
Timur Kim <kimtimur01@gmail.com>
2022-09-03 12:20:26 +0600

    Created a default redux constructor in index.js file. (Frontend)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
+import thunk from "redux-thunk";
+import {Provider} from "react-redux";
 import App from './App';
 import './index.css';
+import {BrowserRouter} from "react-router-dom";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({
+    news: newsReducer,
+    comments: commentsReducer,
+});
+
+const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunk))
+);
+
+const app = (
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App/>
-);
+root.render(app);
